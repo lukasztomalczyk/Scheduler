eb1fe9153c3cf44394192a0af3c746dcdfd11f94
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-05-23 18:54:29 +0200

    work

---
+++ SchedulerQuartez2/ExampleJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace SchedulerQuartez2
+{
+    public class ExampleJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("To jest przykładowe zadanie");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SchedulerQuartez2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace SchedulerQuartez2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+//            for (int i = 0; i < 50; i++)
+//            {
+//               Thread.Sleep(100);
+//                Console.WriteLine(i);
+//            }
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            IScheduler sched = await sf.GetScheduler();
+
+            IJobDetail job = JobBuilder.Create<ExampleJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+            ISimpleTrigger trigger = (ISimpleTrigger) TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(1)
+                .RepeatForever())
+                .Build();
+            
+            await sched.ScheduleJob(job, trigger);
+            Thread.Sleep(1000);
+            Console.ReadKey();
+        }
+    }
+}
