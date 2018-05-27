07ec8123a9dd22e1f48ce8697831177a42e5452c
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-05-27 20:24:08 +0200

    add job listener

---
+++ SchedulerQuartz3/Modules/JobListtener.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+using Quartz;
+
+namespace SchedulerQuartz3.Modules
+{
+    public class JobListtener : IJobListener
+    {
+        private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public Task JobToBeExecuted(IJobExecutionContext context,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task JobExecutionVetoed(IJobExecutionContext context,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task JobWasExecuted(IJobExecutionContext context, JobExecutionException jobException,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            logger.Info("Job Executed.");
+        }
+
+        public string Name => "JobListener";
+    }
+}
